9dcbf1c68478e9b5489dda2be3747177da2ff941
Mosab Sasi <smosab@gmail.com>
2015-10-26 20:15:36 -0600

    adding my answers to exercises in the Variables lesson

---
+++ Intro_to_Programming/Variables/name.rb
@@ -0,0 +1,12 @@
+  puts "Please enter your first name:"
+  firstname = gets.chomp
+  puts "Please enter your last name:"
+  lastname = gets.chomp
+
+  puts "Your Full name is: " + firstname + " " + lastname
+
+  # puts "Hello there "  
+  # 10.times do  
+  #   print name + "! " 
+  # end
+
